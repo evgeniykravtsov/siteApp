4cc854a816cfb72026c38ce0419a791c1670a2c1
Evgeniy Kravtsov <evgeniykravtsov1992@gmail.com>
2022-01-30 19:08:38 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
